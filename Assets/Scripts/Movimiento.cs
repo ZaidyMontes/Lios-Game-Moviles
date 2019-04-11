9f0e3237b426c2aa2126e87ac6ab3bfec3ad8e61
Zaidy Montes <zaydimontes@gmail.com>
2019-04-10 21:36:04 -0500

    Second Repository
    This is the repository of the first prototype.

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -5,14 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Movimiento : MonoBehaviour
 {
-    
+    private int contador = 0;
     public Movimiento movimiento;
     public enum GameState { Idle, Playing };
     public GameState gameState = GameState.Idle;
     //public Vector3 velocidad;
     public float rapidez;
     public float salto;
-   // public float velocidadup;
+    // public float velocidadup;
 
     private Animator animator;
     private Rigidbody2D rigidbody;
@@ -22,17 +22,19 @@ public class Movimiento : MonoBehaviour
     public GameObject canvas;
     public GameObject shot;
     public Transform punto;
-   
+    public GameObject jugador;
+    public Transform puntoInicio;
+
 
     /*public GameObject shot;
     public Transform Punto;*/
 
-    
 
-   // public Transform  
 
-	// Use this for initialization
-	void Start () {
+    // public Transform  
+
+    // Use this for initialization
+    void Start() {
         rigidbody = GetComponent<Rigidbody2D>();
         sensor = GetComponentInChildren<Sensor>();
         sprite = GetComponent<SpriteRenderer>();
@@ -43,36 +45,24 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /* if (gameState == GameState.Idle && Input.GetMouseButtonDown(0))
-        {
-            gameState = GameState.Playing;
-           canvas.SetActive(false);
-        }
-        else if (gameState == GameState.Playing)
-        {
-            Cosa();
+       
 
-        }*/
-    
-    
-    
+   
+
 
         float horizontal = Input.GetAxis("Horizontal");
-        //float vertical = Input.GetAxis("Vertical");
-        //if (transform.position.x < 8) {
-        //transform.position = transform.position + new Vector3(horizontal,vertical,0) * rapidez * Time.deltaTime;
+        
         Vector3 velocidad = rigidbody.velocity;
         velocidad.x = horizontal * rapidez;
+         
+
         if (Input.GetButtonDown("Jump") && sensor.activo)
         { //si presiono la tecla de salto y está sobre el suelo
           //cambio la velocidad en y
             velocidad.y = salto;
         }
 
-        /*if (!sensor.activo)
-        {
-            velocidad.x = velocidad.x * 0.5f;
-        }*/
+        
 
         rigidbody.velocity = velocidad;
         //} else {
@@ -93,7 +83,7 @@ public class Movimiento : MonoBehaviour
             this.mirar = true;
         }
 
-         animator.SetFloat("Rapidez", Mathf.Abs(velocidad.x));
+        animator.SetFloat("Rapidez", Mathf.Abs(velocidad.x));
         animator.SetBool("Suelo", sensor.activo);
         /*if (velocidad.x < 0)
          {
@@ -106,8 +96,8 @@ public class Movimiento : MonoBehaviour
 
 
 
-       // animator
-        
+        // animator
+
         /*if (Input.GetButton("Fire1"))
         {
             Instantiate(shot, Punto.position, Punto.rotation);
@@ -117,28 +107,36 @@ public class Movimiento : MonoBehaviour
     }
 
     void Flip()
+    {
+        this.transform.Rotate(Vector3.up, 180);
+    }
+
+
+    // Jugador se destruye al morir
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Destroid" || col.gameObject.tag == "Enemigo")
         {
-            this.transform.Rotate(Vector3.up, 180);
+            //;Destroy(gameObject);
+            SceneManager.LoadScene("escena1");
+
+
         }
 
-
-        // Jugador se destruye al morir
-        void OnTriggerEnter2D(Collider2D col)
+        if (col.gameObject.tag == "Final")
         {
-            if (col.gameObject.tag == "Destroid" || col.gameObject.tag == "BalaEnemy" || col.gameObject.tag == "Enemigo" || col.gameObject.tag == "Final")
-            {
-            //Destroy(gameObject);
-
             SceneManager.LoadScene("MenuInicio");
-            }
-            else if (col.gameObject.tag == "Final1")
-           {
-            Instantiate(shot, punto.position, punto.rotation);
         }
-         
-        }
-    
+        
+    }
+
+
 
 
 
 }
+    
+
+
+
+
